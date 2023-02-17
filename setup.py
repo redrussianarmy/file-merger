efb681d1c45de5be042ec5483bc213bcf9169b16
Hakan Bogan <hb@hakanbogan.com>
2023-02-17 23:26:49 +0300

    updated setup configurations of the package

--- setup.py
+++ setup.py
@@ -5,14 +5,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="hb-filemerger",
-    version="1.0",
+    name="filemerger",
+    version="1.0.0",
     author="Hakan Bogan",
     author_email="hb@hakanbogan.com",
     description="A CLI tool to merge all input files into a single sorted output file.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_namespace_packages(
+        include=['merge_files', 'merge_files.*']),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -21,7 +22,7 @@ setuptools.setup(
     python_requires='>=3.6',
     entry_points={
         'console_scripts': [
-            'mergefiles=merge_files.merge_files:cli_main',
+            'filemerger=merge_files.main:cli_main',
         ],
     },
 )
